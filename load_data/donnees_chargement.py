65d8472e6ddc85456b5d116e54fb674c38a57e33
gbillon <gustave.billon@eleve.ensai.fr>
2026-04-20 15:58:25 +0000

    ruff + delete stat_explo_parite and chargement_data

--- load_data/donnees_chargement.py
+++ load_data/donnees_chargement.py
@@ -5,23 +5,33 @@ import numpy as np
 
 
 def load_sujet_tele():
-    url_sujet_tele = "https://static.data.gouv.fr/resources/classement-thematique-des-sujets-de-journaux-televises-janvier-2000-decembre-2020/20241015-124725/ina-barometre-jt-tv-donnees-quotidiennes-2000-2020-nbre-sujets-durees-202410.csv"
+    url_sujet_tele = (
+        "https://static.data.gouv.fr/resources/classement" +
+        "-thematique-des-sujets-de-journaux-televises-janvier-2000" +
+        "-decembre-2020/20241015-124725/ina-barometre-jt-tv-donnees" +
+        "-quotidiennes-2000-2020-nbre-sujets-durees-202410.csv"
+    )
     requests.get(url_sujet_tele)
     req = requests.get(url_sujet_tele)
-    colonnes = ["Date", "Chaîne","Vide", "Thématique", "Nb_sujets", "Duree_sec"]
+    colonnes = ["Date", "Chaîne", "Vide", "Thématique", "Nb_sujets", "Duree_sec"]
     with open('temp.csv', 'w', encoding='latin-1') as f:
         f.write(req.text)
     sujet_tele = pd.read_csv('temp.csv', sep=';', encoding='latin-1', header=None, names=colonnes)
     os.remove('temp.csv')
     sujet_tele = sujet_tele.drop(columns=['Vide'])
-    sujet_tele['Date'] = pd.to_datetime(sujet_tele['Date'], dayfirst= True)
-    sujet_tele['Temps_total_JT'] = sujet_tele.groupby(['Date','Chaîne'])['Duree_sec'].transform(sum)
+    sujet_tele['Date'] = pd.to_datetime(sujet_tele['Date'], dayfirst=True)
+    sujet_tele['Temps_total_JT'] = (
+        sujet_tele.groupby(['Date', 'Chaîne'])['Duree_sec'].transform(sum)
+    )
     sujet_tele['Prop'] = sujet_tele['Duree_sec']/sujet_tele['Temps_total_JT']
     return sujet_tele
 
 
 def load_parite():
-    url_parite = "https://static.data.gouv.fr/resources/temps-de-parole-des-hommes-et-des-femmes-a-la-television-et-a-la-radio/20190312-191033/20190308-stats.csv"
+    url_parite = (
+        "https://static.data.gouv.fr/resources/temps-de-parole-des-hommes-et-des-femmes-a-la-" +
+        "television-et-a-la-radio/20190312-191033/20190308-stats.csv"
+    )
     req = requests.get(url_parite)
     with open('temp.csv', 'w', encoding='utf-8') as f:
         f.write(req.text.encode('latin-1').decode('utf-8'))
@@ -33,17 +43,21 @@ def load_parite():
 
 
 def load_audience():
-    req = requests.get("http://www.cnc.fr/c/document_library/get_file?uuid=ac00e68f-871d-4129-ba90-977c84484bdd&groupId=18")
+    req = requests.get(
+        "http://www.cnc.fr/c/document_library/" +
+        "get_file?uuid=ac00e68f-871d-4129-ba90-977c84484bdd&groupId=18"
+    )
     with open('temp.xlsx', 'wb') as f:
         f.write(req.content)
-    audience = pd.read_excel('temp.xlsx',
-                         sheet_name='PartdAudience',
-                         skiprows=5,
-                         header = 1
-                        )
+    audience = pd.read_excel(
+        'temp.xlsx',
+        sheet_name='PartdAudience',
+        skiprows=5,
+        header=1
+    )
     os.remove('temp.xlsx')
     audience = audience.rename(columns={audience.columns[0]: 'Annee'})
-    audience = audience.drop(index=[36,37,38,39])
+    audience = audience.drop(index=[36, 37, 38, 39])
     audience["Annee"] = audience["Annee"].astype(int)
     audience.replace("-", np.nan, inplace=True)
 
